cfe986bab699b388acc61502d4722d1d9a616e40
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-11 19:29:20 -0300

    Montei com grid a table do funcionário

--- client/app.js
+++ client/app.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
   data(){
     return {
-      page: "Início",
+      page: "Funcionário",//"Início",
+      funcionario: "Ele Mesmo",
+      cargo: "Motorista Teste",//remover isso, colocar alguma estrutura melhor
+      mes: "02/2022",
       cargos: [
         { nome: "Manutenção" },
         { nome: "Motorista C" },
@@ -16,6 +19,23 @@ const app = Vue.createApp({
   methods: {
     clickItemMenu(event){
       this.page = event.target.innerText;
+    },
+    clickCargo(event){
+      console.log(event.target.innerText + " foi clicado");
+    },
+    clickFuncionario(event){
+      this.funcionario = event.target.innerText;
+    }
+  },
+  computed: {
+    getDias(funcionario, mes){
+      console.log("getDias "+funcionario+" "+mes);
+      return []
+      //   {
+      //     dia: 1, semana: "TER", t1entrada: "", t1saida: "", intervalo: "0:00", t2entrada: "", t2saida: "", t3entrada: "", t3saida: "",
+      //     total: "", total50: "", total100: "", csabado: "", obs: ""
+      //   }
+      // ];
     }
   }
 });
