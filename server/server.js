72fb6e4b1753937bc831792f08012e6a8d364745
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-15 00:31:14 -0300

    Arrumei a pasta do server-side

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+var app = express();
+//import bodyParser from "body-parser";
+//app.use(bodyParser.urlencoded({ extended: true }));a
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const rootPath = path.dirname(__filename).replace("server", "");
+app.use(express.static(rootPath + "client"));
+app.get("/", (request, response) => {
+    response.sendFile(rootPath + "client\\index.html");
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`O servidor ta rodando na porta ${PORT}.`);
+});
