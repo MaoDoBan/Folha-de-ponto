b8e40d097ed5bd376bc7b9f3357d85bdeb9555c0
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-17 15:41:08 -0300

    Add a gambiarra de tipos do socket.io client

--- src/types/https-import.ts
+++ src/types/https-import.ts
@@ -1,3 +1,7 @@
 declare module 'https://unpkg.com/vue@3/dist/vue.esm-browser.js' {
   export * from 'vue'
 }
+
+declare module 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js' {
+  export * from 'socket.io-client'
+}
