72c3c773228b69dc1d1d5e04e7055e4cf8fcbcce
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-18 14:42:25 -0300

    Add versão placeholder do gerador da planilha do mês de um funcionário

---
+++ system/server/calculaDia.js
@@ -0,0 +1,12 @@
+const semana = ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"];
+let i_semana = 1;
+const fevereiro = []; /// ARRUMAR
+for (let i = 1; i <= 28; i++) {
+    fevereiro.push({
+        numero: i, semana: semana[i_semana], t1entrada: "", t1saída: "", intervalo: "0:00",
+        t2entrada: "", t2saída: "", t3entrada: "", t3saída: "",
+        total: "", total50: "", total100: "", cSábado: "", obs: ""
+    });
+    i_semana = i_semana == 6 ? 0 : i_semana + 1;
+}
+export { fevereiro };
