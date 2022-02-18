e5f3a47f7eb4b5d2969e629e642ca0cb8e168206
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-18 14:44:47 -0300

    Adicionei alguns eventos teste e getMêsFuncionário

--- src/types/socketio.ts
+++ src/types/socketio.ts
@@ -2,10 +2,17 @@ export interface ServerToClientEvents {
   noArg: () => void;
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
+  mensagem: (texto: string) => void;
+  bah: (dados: any) => void;
+  mêsFuncionário: (dados: any) => void;
 }
 
 export interface ClientToServerEvents {
-  hello: () => void;
+  oi: () => void;
+  nah: () => number;
+  mensagem: (texto: string) => void;
+  bah: (dados: any) => void;
+  getMêsFuncionário: (funcionário: string, mês: number, ano: number) => void;
 }
 
 export interface InterServerEvents {
