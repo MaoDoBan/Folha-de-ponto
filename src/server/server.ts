8419cb6087ca0108ac30097421499396ebadaa08
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-02-24 13:50:33 -0300

    server.ts: testando o DB

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,18 @@
 import { httpServer } from "./http.js";
+
+
+import { initDatabase } from "./database/initDatabase.js";
+import { DB } from "./database/DB.js";
+const db = new DB(initDatabase());
+
+console.log("cargos: ", db.getCargos());
+const bah = db.addCargo("Motoristaço PA"); console.log("bah:",bah);
+console.log("cargos: ", db.getCargos());
+db.editCargo(1, "Motorista X");
+console.log("cargos: ", db.getCargos());
+
+
 import { iniciaSocketIo } from "./websocket.js";
-
-
 iniciaSocketIo();
 
 const PORT = process.env.PORT || 3333;
