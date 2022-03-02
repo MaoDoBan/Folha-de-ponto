7dc0dc68b21240b73a88e8f08c065e78a3756e30
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-03-02 13:34:22 -0300

    socketio.ts: identação e ajuste de parâmetros

--- src/types/socketio.ts
+++ src/types/socketio.ts
@@ -3,28 +3,23 @@ import { Funcionário } from "./Funcionário";
 
 
 export interface ServerToClientEvents {
-  sendCargos:      (cargos: Cargo[]) => void;
-  resultAddCargo:  (status: string) => void;
-  resultEditCargo: (status: string) => void;
+  sendCargos:            (cargos: Cargo[]) => void;
+  resultAddCargo:        (status: string) => void;
+  resultEditCargo:       (status: string) => void;
   sendFuncionários:      (funcionários: Funcionário[]) => void;
   resultAddFuncionário:  (status: string) => void;
   resultEditFuncionário: (status: string) => void;
-  mêsFuncionário: (dados: any) => void;///
-  // noArg: () => void;
-  // basicEmit: (a: number, b: string, c: Buffer) => void;
-  // withAck: (d: string, callback: (e: number) => void) => void;
-  // mensagem: (texto: string) => void;
+  mêsFuncionário:        (dados: any) => void;
 }
 
 export interface ClientToServerEvents {
-  getCargos: () => void;
-  addCargo:  (nome: string) => void;
-  editCargo: (nome: string, id: number) => void;
-  getFuncionários: (idCargo: number) => void;
-  addFuncionário:  (nomeFuncionário: string, idCargo: number) => void;
-  editFuncionário: (nome: string, id: number) => void;
-  getMêsFuncionário: (funcionário: string, mês: number, ano: number) => void;///
-  // mensagem: (texto: string) => void;
+  getCargos:         () => void;
+  addCargo:          (nome: string) => void;
+  editCargo:         (id: number, nome: string) => void;
+  getFuncionários:   (idCargo: number) => void;
+  addFuncionário:    (nomeFuncionário: string, idCargo: number) => void;
+  editFuncionário:   (id: number, nome: string) => void;
+  getMêsFuncionário: (funcionário: string, mês: number, ano: number) => void;
 }
 
 export interface InterServerEvents {
@@ -35,3 +30,8 @@ export interface SocketData {
   name: string;
   age: number;
 }
+
+
+  // basicEmit: (a: number, b: string, c: Buffer) => void;
+  // withAck: (d: string, callback: (e: number) => void) => void;
+  // mensagem: (texto: string) => void;
--- system/types/socketio.js
+++ system/types/socketio.js
@@ -1,1 +1,4 @@
 export {};
+// basicEmit: (a: number, b: string, c: Buffer) => void;
+// withAck: (d: string, callback: (e: number) => void) => void;
+// mensagem: (texto: string) => void;
