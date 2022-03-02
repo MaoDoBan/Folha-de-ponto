9ad16bf728f43b93da957ab0a638944136ca616f
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-03-02 14:06:33 -0300

    server: arrumei importação do objeto dados de Dados; add código pra popular tabela Cargos se ela estiver vazia

--- system/server/server.js
+++ system/server/server.js
@@ -1,15 +1,12 @@
 import { httpServer } from "./http.js";
-import { initDatabase } from "./database/initDatabase.js";
-import { Dados } from "./database/Dados.js";
-const dados = new Dados(initDatabase());
-// const bah = dados.database.prepare("SELECT * FROM Cargos WHERE nome = ?;").get('Pagode');
-// console.log("bah:",bah)
-//dados.addCargo("Manutenção");
-// console.log("cargos: ", db.getCargos());
-// const bah = dados.addCargo("Manutenção"); console.log("bah:",bah);
-// console.log("cargos: ", db.getCargos());
-//dados.editCargo(2, "Motorista C");
-// console.log("cargos: ", db.getCargos());
+import { dados } from "./database/Dados.js";
+if (dados.taSemCargos()) { //inicializando cargos
+    const cargos = ["Manutenção", "Motorista C", "Motorista D", "Motorista E", "Operador de Retro", "Operadores Diversos", "Supervisor Operacional"];
+    console.log("Adicionando cargos ao banco de dados:", cargos);
+    for (let cargo of cargos) {
+        dados.addCargo(cargo);
+    }
+}
 import { iniciaSocketIo } from "./websocket.js";
 iniciaSocketIo(dados);
 const PORT = process.env.PORT || 3333;
