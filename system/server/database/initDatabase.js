3b5ac2362fd6c6c95af4ea68d01eaa484122746e
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-03-18 12:14:28 -0300

    initDatabase.ts: condensei checagens se tabela existe pra usar apenas 1 prepare; alterei "levemente" os campos da tabela FolhaDePonto

--- system/server/database/initDatabase.js
+++ system/server/database/initDatabase.js
@@ -1,26 +1,29 @@
 import DatabaseConstructor from "better-sqlite3";
 const database = new DatabaseConstructor("BancoDeDados.db");
+//SELECT name FROM sqlite_master WHERE type='table';
 const sqlStringCheckTableExists = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name = ?;";
 export function initDatabase() {
-    const tabelaCargos = database.prepare(sqlStringCheckTableExists).get('Cargos');
+    const tabelaExiste = database.prepare(sqlStringCheckTableExists);
+    const tabelaCargos = tabelaExiste.get('Cargos');
     if (!tabelaCargos['count(*)']) {
         database.prepare("CREATE TABLE Cargos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT);").run();
         console.log("Banco de dados: criei tabela Cargos");
         database.pragma("synchronous = 1");
         database.pragma("journal_mode = wal");
     }
-    const tabelaFuncionários = database.prepare(sqlStringCheckTableExists).get('Funcionarios');
+    const tabelaFuncionários = tabelaExiste.get('Funcionarios');
     if (!tabelaFuncionários['count(*)']) {
         database.prepare("CREATE TABLE Funcionarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, id_cargo INTEGER NOT NULL REFERENCES Cargos(id));").run();
         console.log("Banco de dados: criei tabela Funcionarios");
     }
-    const tabelaFolhaDePonto = database.prepare(sqlStringCheckTableExists).get('FolhaDePonto');
+    const tabelaFolhaDePonto = tabelaExiste.get('FolhaDePonto');
     if (!tabelaFolhaDePonto['count(*)']) {
         database.prepare("CREATE TABLE FolhaDePonto (" +
-            "id INTEGER PRIMARY KEY AUTOINCREMENT, id_funcionario INTEGER NOT NULL REFERENCES Funcionarios(id), data TEXT, " +
-            "entrada1 TEXT, saida1 TEXT, entrada2 TEXT, saida2 TEXT, entrada3 TEXT, saida3 TEXT, observacao TEXT" +
+            "id_funcionario INTEGER NOT NULL REFERENCES Funcionarios(id), dia INTEGER, mes INTEGER, ano INTEGER, " +
+            "entrada1 TEXT, saida1 TEXT, entrada2 TEXT, saida2 TEXT, entrada3 TEXT, saida3 TEXT, observacao TEXT, " +
+            "CONSTRAINT id PRIMARY KEY (id_funcionario, dia, mes, ano)" +
             ");").run();
-        database.prepare("CREATE UNIQUE INDEX idx_funcionario_data ON FolhaDePonto(id_funcionario, data);").run();
+        database.prepare("CREATE UNIQUE INDEX idx_funcionario_mes_ano ON FolhaDePonto(id_funcionario, mes, ano);").run();
         console.log("Banco de dados: criei tabela FolhaDePonto");
     }
     return database;
