b01a3e10d53f70b316a9da2c12c5d3ae746a96f8
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-03-18 11:47:08 -0300

    add tempo.ts: tempoToMinutos(): transforma "2:33" em 153 minutosToTempo(): faz o oposto da função acima

---
+++ src/server/tempo.ts
@@ -0,0 +1,39 @@
+export function tempoToMinutos(tempo: string){
+  if(tempo == "") return 0;
+
+  let sinal = 1;
+  if(tempo.startsWith("- ")){
+    tempo.replace("- ", "");
+    sinal = -1;
+  }
+
+  const partes = tempo.split(":");//não suporta notação dias:horas:minutos, apenas suporta h:m
+  const horas        = Number(partes.shift());
+  const minutosSobra = Number(partes.shift());
+  return (horas * 60 + minutosSobra) * sinal;
+}
+
+const minutosNaHora = 60 * 24;
+export function minutosToTempo(todosOsMinutos: number){
+  let sinal = "";
+  if(todosOsMinutos < 0){
+    sinal = "- ";
+    todosOsMinutos *= -1;
+  }
+
+  if(todosOsMinutos < 1439){
+    const minutos    = todosOsMinutos % 60;
+    const strMinutos = minutos > 9 ? ""+minutos : "0"+minutos;
+    return sinal + Math.floor(todosOsMinutos / 60) + ":" + strMinutos;
+  }
+
+  const dias = Math.floor(todosOsMinutos / minutosNaHora);
+  const minutosRestantes = todosOsMinutos % minutosNaHora;
+
+  const horas      = Math.floor(minutosRestantes / 60);
+  const strHoras   = horas > 9 ? ""+horas : "0"+horas;
+  const minutos    = minutosRestantes % 60;
+  const strMinutos = minutos > 9 ? ""+minutos : "0"+minutos;
+
+  return sinal + dias + ":" + strHoras + ":" + strMinutos;
+}
